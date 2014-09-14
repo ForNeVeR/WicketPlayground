2a56c3b7f4dcf398ade6474320f3de69d7ae1ecf
Friedrich von Never <friedrich@fornever.me>
2014-09-14 19:18:11 +0700

    Get completely rid of the web.xml.

--- src/main/java/me/fornever/wicketplayground/config/SpringWebAppInitializer.java
+++ src/main/java/me/fornever/wicketplayground/config/SpringWebAppInitializer.java
@@ -1,5 +1,7 @@
 package me.fornever.wicketplayground.config;
 
+import me.fornever.wicketplayground.wicket.WicketApplication;
+import org.apache.wicket.protocol.http.WicketFilter;
 import org.springframework.orm.hibernate4.support.OpenSessionInViewFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
@@ -10,6 +12,7 @@ import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import java.util.EnumSet;
+import java.util.HashMap;
 
 public class SpringWebAppInitializer implements WebApplicationInitializer {
 
@@ -19,6 +22,14 @@ public class SpringWebAppInitializer implements WebApplicationInitializer {
 		appContext.register(ApplicationConfiguration.class);
 
 		servletContext.addListener(new ContextLoaderListener(appContext));
+
+		FilterRegistration.Dynamic wicketFilter = servletContext.addFilter("ru.dcak.MedReg", WicketFilter.class);
+		wicketFilter.setInitParameters(
+				new HashMap<String, String>() {{
+					put("applicationClassName", WicketApplication.class.getCanonicalName());
+					put(WicketFilter.FILTER_MAPPING_PARAM, "/*");
+				}});
+		wicketFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
 
 		FilterRegistration.Dynamic osivFilter = servletContext.addFilter("hibernateFilter", OpenSessionInViewFilter.class);
 		osivFilter.setInitParameter("singleSession", "true");
